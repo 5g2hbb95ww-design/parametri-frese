3b25d50ca1781f9db3412ebec6604e3b802749fa
Antonio <5g2hbb95ww@privaterelay.appleid.com>
2026-07-13 20:33:00 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,36 @@
-const CACHE_NAME = "parametri-frese-v4";
-const FILES_TO_CACHE = [
-  "/parametri-frese/",
-  "/parametri-frese/index.html",
-  "/parametri-frese/style.css",
-  "/parametri-frese/app.js",
-  "/parametri-frese/manifest.json",
-  "/parametri-frese/icon/icon-192.png",
-  "/parametri-frese/icon/icon-512.png",
-  "/parametri-frese/icon/screen-wide.png",
-  "/parametri-frese/icon/screen-mobile.png"
+const CACHE_NAME = 'parametri-frese-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return (
-        cached ||
-        fetch(event.request).catch(() =>
-          caches.match("/parametri-frese/index.html")
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(k => k !== CACHE_NAME)
+                    .map(k => caches.delete(k))
+            )
         )
-      );
-    })
-  );
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(res => {
+            return res || fetch(event.request);
+        })
+    );
 });
