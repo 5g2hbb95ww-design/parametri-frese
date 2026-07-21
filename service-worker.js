6c131144fa9fe3ead740d4f8b01144c4820f9c71
Antonio <5g2hbb95ww@privaterelay.appleid.com>
2026-07-21 23:30:12 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -23,24 +23,6 @@ const FILES_TO_CACHE = [
   "/manifest.json"
 ];
 
-// Install
-self.addEventListener("install", event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-});
-
-// Activate
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
-    )
-  );
-  self.clients.claim();
-});
-
 // Network-first strategy
 self.addEventListener("fetch", event => {
 
