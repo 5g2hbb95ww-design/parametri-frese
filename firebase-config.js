b81249f8e3506783ead5f12dd8756efe3e28df3a
Antonio <5g2hbb95ww@privaterelay.appleid.com>
2026-07-22 10:46:20 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,59 +1,23 @@
-// firebase-config.js
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Import Firebase core
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAZ6lMMVoqzvtREmDW1Ixn0_7qu4QRsvOU",
+    authDomain: "programmazione-3754c.firebaseapp.com",
+    projectId: "programmazione-3754c",
+    storageBucket: "programmazione-3754c.firebasestorage.app",
+    messagingSenderId: "964219306630",
+    appId: "1:964219306630:web:0ef66620b036a6ef920707",
+    measurementId: "G-8B6Z8WRX0M"
+  };
 
-// Import Firestore + CACHE LOCALE
-import {
-  initializeFirestore,
-  persistentLocalCache,
-  persistentMultipleTabManager,
-  collection,
-  addDoc,
-  getDocs,
-  query,
-  where,
-  updateDoc,
-  deleteDoc,
-  doc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-// Configurazione Firebase del tuo progetto
-const firebaseConfig = {
-  apiKey: "AIzaSyAZ6lMMVoqzvtREmDW1Ixn0_7qu4QRsvOU",
-  authDomain: "programmazione-3754c.firebaseapp.com",
-  projectId: "programmazione-3754c",
-  storageBucket: "programmazione-3754c.firebasestorage.app",
-  messagingSenderId: "964219306630",
-  appId: "1:964219306630:web:0ef66620b036a6ef920707",
-  measurementId: "G-8B6Z8WRX0M"
-};
-
-// Inizializza Firebase
-const app = initializeApp(firebaseConfig);
-
-// =============================
-// FIRESTORE CON CACHE LOCALE
-// =============================
-const db = initializeFirestore(app, {
-  localCache: persistentLocalCache({
-    tabManager: persistentMultipleTabManager()
-  })
-});
-
-// Utente fisso (per ora)
-const CURRENT_USER = "antonio";
-
-// Esporta tutto per app.js
-export {
-  db,
-  collection,
-  addDoc,
-  getDocs,
-  query,
-  where,
-  updateDoc,
-  deleteDoc,
-  doc,
-  CURRENT_USER
-};
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
